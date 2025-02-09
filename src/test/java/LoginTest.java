a65b90fc94ebce76e103596601affaf6eb17de6d
Merve Gül ATALAY <mervegulatly7@gmail.com>
2025-02-09 20:43:14 +0300

    new page add

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,12 +1,14 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import org.openqa.selenium.WebElement;
+
 import java.io.ByteArrayOutputStream;
+import java.time.Duration;
+import java.util.List;
 
 public class LoginTest {
 
@@ -15,16 +17,40 @@ public class LoginTest {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.hepsiburada.com/");
         driver.manage().window().maximize();
-        driver.findElement(By.cssSelector("[class = 'initialComponent-hk7c_9tvgJ8ELzRuGJwC']")).click();
+        driver.findElement(By.cssSelector("[class = 'initialComponent-hk7c_9tvgJ8ELzRuGJwC']")).click(); //search button clicked
         Thread.sleep(3000);
-        driver.findElement(By.cssSelector("[class = 'searchBarContent-UfviL0lUukyp5yKZTi4k']")).sendKeys("t-shirt");
+        driver.findElement(By.cssSelector("[class = 'searchBarContent-UfviL0lUukyp5yKZTi4k']")).sendKeys("laptop"); //a laptop is typed
+        Thread.sleep(3000);
+        driver.findElements(By.cssSelector("[class = 'suggestion-lhRLulu_2T1ZBMfVLv1X']")).get(1).click(); //word choice
+        Thread.sleep(3000);
+       // driver.findElements(By.cssSelector("[class = 'moria-ProductCard-jmtwOA expP swnvhjgo9zm']")).get(0).click(); //image choice
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0, 500)"); //500px scroll down
+
+        List<WebElement> products = driver.findElements(By.cssSelector("[class = 'productListContent-zAP0Y5msy8OHn5z7T_K_']")); //all products selected
+
+        if (products.size() > 0) {  //first product clicked
+            products.get(0).click();
+        }
         Thread.sleep(3000);
 
-        driver.findElements(By.cssSelector("[class = 'suggestion-lhRLulu_2T1ZBMfVLv1X']")).get(0).click();
+        WebElement addToCartButton = driver.findElement(By.cssSelector("[class='VZMbm89fzHuumKKSNdPb QQT2ceznooRrb4cvyd05']")); // find item
+        js.executeScript("arguments[0].scrollIntoView();", addToCartButton); //Use scrollIntoView() to make the element visible
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10)); //Wait for the item to be clickable
+        wait.until(ExpectedConditions.elementToBeClickable(addToCartButton));
 
+        addToCartButton.click(); // click on cart
+
+        Assert.assertEquals(driver.findElement(By.cssSelector("[class = 'checkoutui-ProductOnBasketHeader-nOvp_U8bHbLzgKbSUFaz']")), " Ürün sepetinizde");
+
+        driver.findElement(By.cssSelector("[class = 'checkoutui-Modal-iHhyy79iR28NvF33vKJb']")).click();
+
+        driver.findElement(By.id("shoppingCart")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.id("continue_step_btn")).click();
         Thread.sleep(3000);
 
-        driver.findElements(By.cssSelector("[class = 'moria-ProductCard-exfLof eMjQ so6v7ay124z']")).get(0).click();
 
         //driver.quit();
     }
